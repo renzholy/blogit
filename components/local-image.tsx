5d93f0dd2989f732ecf430dd04e089ba02ac43f0
Zehua Ren <renzholy@gmail.com>
2021-05-17 00:03:29 +0800

    resolve relative image path

--- components/local-image.tsx
+++ components/local-image.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
+import { useRouter } from 'next/dist/client/router'
 import { useMemo } from 'react'
 import { ComponentProps } from 'rehype-react'
+import { resolve, dirname } from 'path'
 
 export default function LocalImage(
   props: ComponentProps & {
@@ -9,8 +11,14 @@ export default function LocalImage(
     src?: string
   },
 ) {
+  const router = useRouter()
+  const path = router.query.path as string[] | undefined
   const src = useMemo(() => {
-    if (props.src!.startsWith('http') || props.src!.startsWith('//')) {
+    if (
+      !props.src ||
+      props.src.startsWith('http') ||
+      props.src.startsWith('//')
+    ) {
       return props.src
     }
     if (!process.env.NEXT_PUBLIC_OWNER || !process.env.NEXT_PUBLIC_REPO) {
@@ -20,8 +28,8 @@ export default function LocalImage(
       process.env.NEXT_PUBLIC_OWNER
     }/${process.env.NEXT_PUBLIC_REPO}/${
       process.env.NEXT_PUBLIC_BRANCH
-    }${props.src!.replace(/^\.\//, '/')}`
-  }, [props.src])
+    }${resolve(dirname(`/${path?.join('/') || ''}`), props.src)}`
+  }, [path, props.src])
 
   return <img {...props} alt={props.alt} src={src} />
 }
