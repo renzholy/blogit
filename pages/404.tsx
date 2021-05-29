55183ff43ef54f8e6296147c5be126cd61947ce5
Zehua Ren <renzholy@gmail.com>
2021-05-30 01:30:21 +0800

    random cat

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,34 @@
 import { css } from '@linaria/core'
+import useSWR from 'swr'
 
 export default function NotFound() {
+  const { data } = useSWR<{ file: string }>(
+    'https://aws.random.cat/meow',
+    (url) => fetch(url).then((response) => response.json()),
+    { revalidateOnFocus: false },
+  )
+
   return (
     <>
       <div
         className={css`
           margin: 96px auto;
           width: fit-content;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
         `}>
         404
+        {data ? (
+          <img
+            src={data.file}
+            alt="random cat"
+            className={css`
+              margin-top: 32px;
+              max-width: 90%;
+            `}
+          />
+        ) : null}
       </div>
     </>
   )
